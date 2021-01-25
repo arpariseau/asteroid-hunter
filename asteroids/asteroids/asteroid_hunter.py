d88840a79a79872b94d9ddf667afec8690b07932
Alex Pariseau <arpariseau@gmail.com>
2021-01-24 18:53:55 -0700

    Can now do closest approach for all asteroids

--- asteroids/asteroids/asteroid_hunter.py
+++ asteroids/asteroids/asteroid_hunter.py
@@ -4,15 +4,15 @@ import requests
 
 def asteroid_closest_approach():
     all_asteroids = []
-    # req = requests.get(f'https://api.nasa.gov/neo/rest/v1/neo/browse?api_key={os.environ["POETRY_NASA_API_KEY"]}')
-    # pages = req.json()['page']['total_pages']
-    # for pg in range(0, pages):
-    asteroids = requests.get(f'http://www.neowsapp.com/rest/v1/neo/browse?page=0&size=20&api_key={os.environ["POETRY_NASA_API_KEY"]}')
-    for ast in range(0, 20):
-        approaches = asteroids.json()['near_earth_objects'][ast]['close_approach_data']
-        closest_approach = min(approaches, key=lambda x:x['miss_distance']['astronomical'])
-        all_asteroids.append(asteroids.json()['near_earth_objects'][ast])
-        all_asteroids[ast]['close_approach_data'] = [closest_approach]
+    req = requests.get(f'https://api.nasa.gov/neo/rest/v1/neo/browse?api_key={os.environ["POETRY_NASA_API_KEY"]}')
+    pages = req.json()['page']['total_pages']
+    for pg in range(0, pages):
+        asteroids = requests.get(f'http://www.neowsapp.com/rest/v1/neo/browse?page={pg}&size=20&api_key={os.environ["POETRY_NASA_API_KEY"]}')
+        for ast in range(0, 20):
+            approaches = asteroids.json()['near_earth_objects'][ast]['close_approach_data']
+            closest_approach = min(approaches, key=lambda x:x['miss_distance']['astronomical'])
+            all_asteroids.append(asteroids.json()['near_earth_objects'][ast])
+            all_asteroids[ast]['close_approach_data'] = [closest_approach]
     return json.dumps(all_asteroids)
 
 
--- asteroids/tests/test_asteroids.py
+++ asteroids/tests/test_asteroids.py
@@ -3,7 +3,15 @@ from asteroids.asteroid_hunter import asteroid_closest_approach
 # import asteroids.asteroid_hunter.month_closest_approaches
 # import asteroids.asteroid_hunter.nearest_misses
 
+#for these tests to work, you need to set pages = 5 in the method - because of
+#how many pages there are in the data, didn't want to end up limiting my api key
 def test_only_one_closest_approach():
     result = json.loads(asteroid_closest_approach())[0]
     assert 'close_approach_data' in result
     assert len(result['close_approach_data']) == 1
+
+def test_multiple_pages_of_closest_approach():
+    result = json.loads(asteroid_closest_approach())
+    assert len(result) == 100
+    assert result[0]['id'] == '2000433'
+    assert result[-1]['id'] == '2005660'
