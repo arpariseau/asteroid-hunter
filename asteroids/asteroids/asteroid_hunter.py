ef64a29bd493f0a3fb99d9234562e5a20a7685e5
Alex Pariseau <arpariseau@gmail.com>
2021-01-24 20:06:25 -0700

    Can now do monthly closest approaches

--- asteroids/asteroids/asteroid_hunter.py
+++ asteroids/asteroids/asteroid_hunter.py
@@ -6,18 +6,26 @@ def asteroid_closest_approach():
     all_asteroids = []
     req = requests.get(f'https://api.nasa.gov/neo/rest/v1/neo/browse?api_key={os.environ["POETRY_NASA_API_KEY"]}')
     pages = req.json()['page']['total_pages']
-    for pg in range(0, pages):
+    for pg in range(0, 5):
+    #set to range(0, pages) for all data - didn't want to rate limit while testing
         asteroids = requests.get(f'http://www.neowsapp.com/rest/v1/neo/browse?page={pg}&size=20&api_key={os.environ["POETRY_NASA_API_KEY"]}')
         for ast in range(0, 20):
             approaches = asteroids.json()['near_earth_objects'][ast]['close_approach_data']
             closest_approach = min(approaches, key=lambda x:x['miss_distance']['astronomical'])
             all_asteroids.append(asteroids.json()['near_earth_objects'][ast])
-            all_asteroids[ast]['close_approach_data'] = [closest_approach]
+            all_asteroids[((pg * 20) + ast)]['close_approach_data'] = [closest_approach]
     return json.dumps(all_asteroids)
 
 
-#def month_closest_approaches():
-
-
+def month_closest_approaches():
+    element_count = 0
+    near_earth_objects = {}
+    calendar = [['01-01','01-08'], ['01-09','01-15'], ['01-16','01-22'], ['01-23','01-29'], ['01-30','01-31']]
+    for week in calendar:
+        weekly_approaches = requests.get(f'https://api.nasa.gov/neo/rest/v1/feed?start_date=2021-{week[0]}&end_date=2021-{week[1]}&api_key={os.environ["POETRY_NASA_API_KEY"]}')
+        element_count += weekly_approaches.json()['element_count']
+        near_earth_objects.update(weekly_approaches.json()['near_earth_objects'])
+    month_approaches = {'element_count' : element_count} | {'near_earth_objects' : near_earth_objects}
+    return json.dumps(month_approaches)
 
 #def nearest_misses():
--- asteroids/tests/test_asteroids.py
+++ asteroids/tests/test_asteroids.py
@@ -1,17 +1,31 @@
 import json
 from asteroids.asteroid_hunter import asteroid_closest_approach
-# import asteroids.asteroid_hunter.month_closest_approaches
+from asteroids.asteroid_hunter import month_closest_approaches
 # import asteroids.asteroid_hunter.nearest_misses
 
 #for these tests to work, you need to set pages = 5 in the method - because of
 #how many pages there are in the data, didn't want to end up limiting my api key
 def test_only_one_closest_approach():
     result = json.loads(asteroid_closest_approach())[0]
-    assert 'close_approach_data' in result
     assert len(result['close_approach_data']) == 1
+
+def test_is_minimum_closest_approach():
+    result = json.loads(asteroid_closest_approach())[0]
+    assert result['close_approach_data'][0]['miss_distance']['astronomical'] == '0.1494623118'
 
 def test_multiple_pages_of_closest_approach():
     result = json.loads(asteroid_closest_approach())
     assert len(result) == 100
     assert result[0]['id'] == '2000433'
     assert result[-1]['id'] == '2005660'
+
+def test_all_days_in_month_closest_approaches():
+    result = json.loads(month_closest_approaches())
+    assert len(result['near_earth_objects']) == 31
+
+def test_correct_element_count_in_month_closest_approaches():
+    result = json.loads(month_closest_approaches())
+    sum_of_days = 0
+    for day in result['near_earth_objects']:
+        sum_of_days += len(result['near_earth_objects'][day])
+    assert result['element_count'] == sum_of_days
