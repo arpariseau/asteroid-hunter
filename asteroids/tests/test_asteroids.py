ce410d89c7a5216180b41b21d20211d42907b431
Alex Pariseau <arpariseau@gmail.com>
2021-01-24 18:40:01 -0700

    Works for one page

--- asteroids/tests/test_asteroids.py
+++ asteroids/tests/test_asteroids.py
@@ -1,5 +1,9 @@
-from asteroids import __version__
+import json
+from asteroids.asteroid_hunter import asteroid_closest_approach
+# import asteroids.asteroid_hunter.month_closest_approaches
+# import asteroids.asteroid_hunter.nearest_misses
 
-
-def test_version():
-    assert __version__ == '0.1.0'
+def test_only_one_closest_approach():
+    result = json.loads(asteroid_closest_approach())[0]
+    assert 'close_approach_data' in result
+    assert len(result['close_approach_data']) == 1
